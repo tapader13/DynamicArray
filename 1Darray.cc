3e1aff3c3a151843ac9df773af53fc70df0159a6
tapader13 <minhajtapader0@gmail.com>
2023-05-23 00:07:26 -0400

    first commit

---
+++ 1Darray.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // 1D memory allocation---------------->
+    int *arr = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    delete[] arr;
+}
